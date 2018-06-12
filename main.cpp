11873ec6702b6c32fb601649fc55d1d09e30c30a
Sajti Martin <sajti.martin@gmail.com>
2018-06-13 00:26:50 +0200

    getting the trees

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void input()
+{
+	int mezo[101][101];
+	int sor=0,oszlop=0;
+	int vmi=0;
+	cin>>sor>>oszlop;
+	for(int i=0;i<oszlop-1;i++)
+	{
+		for(int j=0;j<sor-1;j++)
+		{
+			cin>>vmi;
+			mezo[j][i]=vmi;
+		}
+	}
+}
 int main()
 {
+	int kivag=0;
+	input();
 	return 0;
 }
